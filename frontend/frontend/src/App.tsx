64dd88a8b3c01dd4f59064f380f3829e8ec13af6
Ashdeep-Singh-97 <ashdeepsingh1997@gmail.com>
2024-11-08 14:42:14 +0530

    HashRouter added

--- frontend/frontend/src/App.tsx
+++ frontend/frontend/src/App.tsx
@@ -1,23 +1,14 @@
-
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Page from "./components/Page";
-import { BrowserRouter as Router, Routes, Route  } from "react-router-dom";
-import { gsap } from "gsap";
-import { useGSAP } from '@gsap/react';
 import BouncingBall from "./components/BouncingBall";
 
 export default function App() {
-
-  useGSAP(() => {
-    gsap.to('.link', { opacity: 1, y: -100, delay: 2 })
-  }, [])
-
   return (
     <Router>
-            <Routes>
-                <Route path="/" element={<BouncingBall />} />
-                <Route path="/product" element={<Page />} />
-            </Routes>
-        </Router>
+      <Routes>
+        <Route path="/" element={<BouncingBall />} />
+        <Route path="/product" element={<Page />} />
+      </Routes>
+    </Router>
   );
 }
-
